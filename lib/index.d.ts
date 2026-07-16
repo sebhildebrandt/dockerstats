7fbeab1bd03ef510d803dbd96d1206b57951008a
Sebastian Hildebrandt <hildebrandt@plus-innovations.com>
2026-07-16 20:25:52 +0200

    typescript update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -158,12 +158,37 @@ export namespace Dockerstats {
     scope: string;
     created: number;
   }
+
+  interface DockerAllData extends DockerContainerData {
+    memUsage: number;
+    memLimit: number;
+    memPercent: number;
+    cpuPercent: number;
+    pids: number;
+    netIO: {
+      rx: number;
+      wx: number;
+    };
+    blockIO: {
+      r: number;
+      w: number;
+    };
+    cpuStats: any;
+    precpuStats: any;
+    memoryStats: any;
+    networks: any;
+    processes: DockerContainerProcessData[];
+  }
 }
 
+export function version(): string;
 export function dockerInfo(cb?: (data: Dockerstats.DockerInfoData) => any): Promise<Dockerstats.DockerInfoData>;
+export function dockerImages(cb: (data: Dockerstats.DockerImageData[]) => any): Promise<Dockerstats.DockerImageData[]>;
 export function dockerImages(all?: boolean, cb?: (data: Dockerstats.DockerImageData[]) => any): Promise<Dockerstats.DockerImageData[]>;
+export function dockerContainers(cb: (data: Dockerstats.DockerContainerData[]) => any): Promise<Dockerstats.DockerContainerData[]>;
 export function dockerContainers(all?: boolean, cb?: (data: Dockerstats.DockerContainerData[]) => any): Promise<Dockerstats.DockerContainerData[]>;
+export function dockerContainerStats(cb: (data: Dockerstats.DockerContainerStatsData[]) => any): Promise<Dockerstats.DockerContainerStatsData[]>;
 export function dockerContainerStats(id?: string, cb?: (data: Dockerstats.DockerContainerStatsData[]) => any): Promise<Dockerstats.DockerContainerStatsData[]>;
-export function dockerContainerProcesses(id?: string, cb?: (data: any) => any): Promise<Dockerstats.DockerContainerProcessData[]>;
+export function dockerContainerProcesses(id?: string, cb?: (data: Dockerstats.DockerContainerProcessData[]) => any): Promise<Dockerstats.DockerContainerProcessData[]>;
 export function dockerVolumes(cb?: (data: Dockerstats.DockerVolumeData[]) => any): Promise<Dockerstats.DockerVolumeData[]>;
-export function dockerAll(cb?: (data: any) => any): Promise<any>;
+export function dockerAll(cb?: (data: Dockerstats.DockerAllData[]) => any): Promise<Dockerstats.DockerAllData[]>;
